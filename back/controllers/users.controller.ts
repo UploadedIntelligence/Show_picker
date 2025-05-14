a37bb1a294cf3cc782fb361d9fd0dde1a9784f04
kakvo <kakvoo1@gmail.com>
2025-05-14 16:30:52 +0100

    user can register

--- back/controllers/users.controller.ts
+++ back/controllers/users.controller.ts
@@ -1,8 +1,21 @@
 import {queryRegister} from "../models/users.model";
+import { Request, Response } from 'express'
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
-function registerUser(req, res) {
+async function registerUser(req: Request, res: Response) {
     const { email, username, password } = req.body
+    console.log('here')
 
+    const user = await prisma.users.create( {
+        data: {
+            email: email,
+            username: username,
+            password: password
+        }
+    })
+
+    res.status(204).json({user})
 }
 
 export { registerUser }
--- back/endpoints.ts
+++ back/endpoints.ts
@@ -1,9 +1,10 @@
-import * as express from 'express'
+import express from 'express'
 import { registerUser } from "./controllers/users.controller";
 
 const app = express()
 
-app.get('/register', registerUser)
+app.use(express.json())
+app.post('/register', registerUser)
 
 app.listen(9000, () => {
     console.log('Now listening port 9000')

ec76fd170f27f71ae9ab475c282a30f49c8613a8
kakvo <kakvoo1@gmail.com>
2025-05-17 23:50:46 +0100

    Added routes for different components, login endpoint no longer returns token, instead returns the user data. Moved dependencies to devdependencies

--- front/show_picker/src/App.tsx
+++ front/show_picker/src/App.tsx
@@ -1,19 +1,56 @@
-import React, {useState, useEffect} from 'react'
-import './App.css'
-import {UserRegistration} from "./UserRegistration.tsx";
-import {Login} from "./Login.tsx";
+import { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { MainPage } from "./MainPage.tsx";
+import { Login } from "./Login.tsx";
+import { UserRegistration } from "./UserRegistration.tsx";
+import axios from "axios";
+import {
+  CurrentUserContext,
+  type ICurrentUserContext,
+} from "./contexts/current-user-context.ts";
 
 function App() {
+  const [loggedUser, setLoggedUser] = useState<
+    ICurrentUserContext["loggedUser"] | null
+  >(null);
 
-    return (
-        <>
-            <h1>Show Picker</h1>
-            <h2>Register</h2>
-            <UserRegistration/>
-            <h2>Login</h2>
-            <Login/>
-        </>
-    )
+  useEffect(() => {
+    async function fetchUser() {
+      await axios
+        .get<{
+          user: {
+            username: string;
+            email: string;
+            id: number;
+            iat: number;
+            exp: number;
+          };
+        }>("http://localhost:9000/user", { withCredentials: true })
+        .then((res) =>
+          setLoggedUser({
+            username: res.data.user.username,
+            email: res.data.user.email,
+            id: res.data.user.id,
+          }),
+        );
+    }
+
+    fetchUser();
+  }, []);
+
+  return (
+    <CurrentUserContext.Provider value={{ loggedUser }}>
+      <Routes>
+        <Route path="" element={<MainPage />}></Route>
+        <Route
+          path="/login"
+          element={<Login setLoggedUser={setLoggedUser} />}
+        />
+        <Route path="/register" element={<UserRegistration />} />
+      </Routes>
+    </CurrentUserContext.Provider>
+  );
 }
 
-export default App
+export default App;
--- front/show_picker/src/Login.tsx
+++ front/show_picker/src/Login.tsx
@@ -1,37 +1,46 @@
-import React, {useState} from "react";
-import axios from 'axios'
+import React, { useState } from 'react';
+import axios from 'axios';
+import type { ICurrentUserContext } from './contexts/current-user-context.ts';
 
+interface LoggedUserProps {
+    setLoggedUser: React.Dispatch<React.SetStateAction<ICurrentUserContext['loggedUser'] | null>>;
+}
 
-function Login() {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [userToken, setUserToken] = useState(null)
-
-    function updateEmail(event: React.ChangeEvent<HTMLInputElement>) {
-        setEmail(event.target.value)
-    }
-
-    function updatePassword(event: React.ChangeEvent<HTMLInputElement>) {
-        setPassword(event.target.value)
-    }
+function Login({ setLoggedUser }: LoggedUserProps) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
     function logAttempt() {
-        axios.post('http://localhost:9000/login', {
-            email, password
-        }).then(result =>
-            setUserToken(result)
-        )
+        axios
+            .post<{
+                email: string;
+                username: string;
+                id: number;
+            }>(
+                'http://localhost:9000/login',
+                {
+                    email,
+                    password,
+                },
+                { withCredentials: true },
+            )
+            .then((result) => {
+                setLoggedUser(result.data);
+            });
     }
 
     return (
-        <form className='login' action={logAttempt}>
+        <form className="login" action={logAttempt}>
+            <h2>Login</h2>
             <label> Email: </label>
-            <input type='text' onChange={updateEmail}/>
+            <input type="text" onChange={(event) => setEmail(event.target.value)} />
             <label> Password: </label>
-            <input type='text' onChange={updatePassword}/>
-            <button className='submit_log_btn' type='submit'>Submit</button>
+            <input type="text" onChange={(event) => setPassword(event.target.value)} />
+            <button className="submit_log_btn" type="submit">
+                Submit
+            </button>
         </form>
-    )
+    );
 }
 
-export { Login }
+export { Login };

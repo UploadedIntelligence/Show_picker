f1e8ab00b72bcd23ee4c805e46977268b9b54894
kakvo <kakvoo1@gmail.com>
2025-06-05 21:25:30 +0100

    Can now look for trailers through youtube data api. Added new endpoint for it.

--- back/endpoints.ts
+++ back/endpoints.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { getUser, logOut, logUser, registerUser, searchIMDB } from './controllers/users.controller';
+import { getUser, logOut, logUser, registerUser, searchIMDB, youtubeAPI } from './controllers/users.controller';
 import cookieParser from 'cookie-parser';
 
 const app = express();
@@ -13,6 +13,7 @@ app.post('/logout', logOut);
 app.post('/register', registerUser);
 app.post('/login', logUser);
 app.post('/search', searchIMDB);
+app.get('/youtube/:search_term', youtubeAPI);
 
 app.listen(process.env.APP_PORT, () => {
     console.log(`Now listening port ${process.env.APP_PORT}`);

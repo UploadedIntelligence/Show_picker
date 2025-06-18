2374b281ccd4a4f5757d15cb23b3d8c258afebb3
kakvo <kakvoo1@gmail.com>
2025-06-18 14:35:49 +0100

    user now prompted for providing geo location; accepting adds it to the context provider

--- front/show_picker/src/app.tsx
+++ front/show_picker/src/app.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './app.css';
 import { UserLogTest } from './components/user-log-test.tsx';
@@ -11,7 +11,8 @@ import { NavBar } from './components/nav-bar.tsx';
 import { ThemeProvider } from '@emotion/react';
 import { green } from '@mui/material/colors';
 import { SearchResult } from './components/search-result.tsx';
-import { user } from './api/user.ts';
+import { fetchUser } from './api/fetchUser.ts';
+import { GeoLocationContext } from './contexts/geo-location-context.ts';
 
 function App() {
     const theme = createTheme({
@@ -33,27 +34,37 @@ function App() {
     });
 
     const [loggedUser, setLoggedUser] = useState<ICurrentUserContext['loggedUser'] | null>(null);
+    const [geoLocation, setGeoLocation] = useState<string | null>(null);
 
     async function initialiseUser() {
-        setLoggedUser((await user()).user);
+        setLoggedUser((await fetchUser()).user);
     }
 
     function logOut() {
         setLoggedUser(null);
     }
 
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            const { latitude, longitude } = position.coords;
+            setGeoLocation(`${latitude};${longitude}`);
+        });
+    }, []);
+
     return (
         <ThemeProvider theme={theme}>
-            <CurrentUserContext.Provider value={{ loggedUser }}>
-                <NavBar isAuthenticated={!!loggedUser} />
-                <Routes>
-                    <Route path="" element={<UserLogTest />}></Route>
-                    <Route path="/login" element={<LoginPage onSuccessfulLogin={initialiseUser} />} />
-                    <Route path="/logout" element={<LogoutPage onLogOut={logOut} />} />
-                    <Route path="/register" element={<UserRegistrationPage onRegister={initialiseUser} />} />
-                    <Route path="/search" element={<SearchResult />} />
-                </Routes>
-            </CurrentUserContext.Provider>
+            <GeoLocationContext.Provider value={geoLocation}>
+                <CurrentUserContext.Provider value={{ loggedUser }}>
+                    <NavBar isAuthenticated={!!loggedUser} />
+                    <Routes>
+                        <Route path="" element={<UserLogTest />}></Route>
+                        <Route path="/login" element={<LoginPage onSuccessfulLogin={initialiseUser} />} />
+                        <Route path="/logout" element={<LogoutPage onLogOut={logOut} />} />
+                        <Route path="/register" element={<UserRegistrationPage onRegister={initialiseUser} />} />
+                        <Route path="/search" element={<SearchResult />} />
+                    </Routes>
+                </CurrentUserContext.Provider>
+            </GeoLocationContext.Provider>
         </ThemeProvider>
     );
 }

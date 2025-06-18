181ecd159452ec03427373aba19b471f410b5f54
kakvo <kakvoo1@gmail.com>
2025-06-18 14:27:48 +0100

    Added search for cinema url through google api and movieglu search that lists the closest 4 cinemas relative to the geolocation of the user

--- back/controllers/search.controller.ts
+++ back/controllers/search.controller.ts
@@ -3,20 +3,81 @@ import axios from 'axios';
 
 const env = require('dotenv').config({ path: `${__dirname}/../config/dev.env` });
 
-async function searchIMDB(req: Request, res: Response) {
-    const { search_term } = req.params;
+async function googleCinema(req: Request, res: Response) {
+    const { cinema_query, geo_location } = req.params;
 
-    const options = {
-        method: 'GET',
-        url: `https://imdb-movies-web-series-etc-search.p.rapidapi.com/${search_term}.json`,
-        headers: {
-            'x-rapidapi-key': `${env.parsed.IMDB_SEARCH_KEY}`,
-            'x-rapidapi-host': `${env.parsed.IMDB_SEARCH_HOST}`,
-        },
+    const cinema_url = await axios
+        .get('https://customsearch.googleapis.com/customsearch/v1', {
+            params: {
+                key: env.parsed.GOOGLE_SEARCH_KEY,
+                cx: env.parsed.GOOGLE_ENGINE_ID,
+                q: cinema_query,
+                gl: geo_location,
+                num: 1,
+                hl: 'en',
+            },
+        })
+        .then((res) => res.data.items[0].link);
+
+    res.status(200).json(cinema_url);
+}
+
+async function searchMovieGlu(req: Request, res: Response) {
+    const { movie_name, geo_location } = req.params;
+
+    const date_today = new Date().toISOString().slice(0, 10);
+
+    const glu_headers = {
+        'x-api-key': env.parsed.MOVIEGLU_API_KEY,
+        authorization: env.parsed.MOVIEGLU_AUTH,
+        geolocation: '-22.0;14.0',
+        territory: 'XX',
+        'api-version': 'v201',
+        client: env.parsed.MOVIEGLU_CLIENT,
+        'device-datetime': date_today,
     };
 
     try {
-        const response = await axios.request(options);
+        const movie_id = await axios
+            .get('https://api-gate2.movieglu.com/filmLiveSearch', {
+                headers: glu_headers,
+                params: {
+                    query: movie_name,
+                    n: '1',
+                },
+            })
+            .then((res) => res.data.films[0].film_id);
+
+        const cinemas = await axios
+            .get('https://api-gate2.movieglu.com/filmShowTimes', {
+                headers: glu_headers,
+                params: {
+                    film_id: movie_id,
+                    n: '4',
+                    date: date_today,
+                },
+            })
+            .then((res) => res.data.cinemas);
+
+        res.status(200).json(cinemas);
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to fetch from MovieGlu' });
+    }
+}
+
+async function searchIMDB(req: Request, res: Response) {
+    const { search_term } = req.params;
+
+    try {
+        const response = await axios.get(
+            `https://imdb-movies-web-series-etc-search.p.rapidapi.com/${search_term}.json`,
+            {
+                headers: {
+                    'x-rapidapi-key': env.parsed.IMDB_SEARCH_KEY,
+                    'x-rapidapi-host': env.parsed.IMDB_SEARCH_HOST,
+                },
+            },
+        );
         const data = response.data.d;
         res.status(200).json(data);
     } catch (error) {
@@ -30,9 +91,9 @@ async function youtubeAPI(req: Request, res: Response) {
     const response = await axios.get(`https://www.googleapis.com/youtube/v3/search`, {
         params: {
             part: 'snippet',
-            q: `${search_term}`,
+            q: search_term,
             type: 'video',
-            key: env.parsed.GOOGLE_SEARCH_API,
+            key: env.parsed.GOOGLE_SEARCH_KEY,
             relevanceLanguage: 'en',
         },
     });
@@ -57,7 +118,7 @@ async function omdbAPI(req: Request, res: Response) {
     const response = await axios.get('https://www.omdbapi.com/', {
         params: {
             apikey: env.parsed.OMDB_API_KEY,
-            i: `${show_id}`,
+            i: show_id,
             plot: 'full',
         },
     });
@@ -65,4 +126,4 @@ async function omdbAPI(req: Request, res: Response) {
     res.send(response.data);
 }
 
-export { searchIMDB, youtubeAPI, watchMode, omdbAPI };
+export { searchIMDB, youtubeAPI, watchMode, omdbAPI, searchMovieGlu, googleCinema };
